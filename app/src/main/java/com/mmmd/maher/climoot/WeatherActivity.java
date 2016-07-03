63b9c24be66f5346669bd6881f0bfaeea4a4ee7f
maher <matt@gommmd.com>
2016-07-03 12:00:54 -0400

    Get user location

--- app/src/main/java/com/mmmd/maher/climoot/WeatherActivity.java
+++ app/src/main/java/com/mmmd/maher/climoot/WeatherActivity.java
@@ -1,31 +1,54 @@
 package com.mmmd.maher.climoot;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
-    final String URL_COORDS = "/?lat=33.5967815&lon=-83.8601827";
+    final String URL_COORDS = "/?lat=";  //"/?lat=33.5967815&lon=-83.8601827";
     final String URL_UNITS = "&units=imperial";
     final String URL_API_KEY = "&APPID=1a572d94466740559cafd15dcfd262d3";
 
+    private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSION_LOCATION = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        final String url = URL_BASE + URL_COORDS + URL_UNITS + URL_API_KEY;
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    public void downloadWeatherData(Location location) {
+        final String fullCoords = URL_COORDS + location.getLatitude() + "&lon=" + location.getLongitude();
+        final String url = URL_BASE + fullCoords + URL_UNITS + URL_API_KEY;
 
         final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -40,5 +63,63 @@ public class WeatherActivity extends AppCompatActivity {
         });
 
         Volley.newRequestQueue(this).add(jsonRequest);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        downloadWeatherData(location);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION);
+
+            Log.v("MY_MAPS", "Requestion Permissions");
+        } else {
+            Log.v("MY_MAPS", "Starting Locations Services from onConnected");
+            startLocationServices();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void startLocationServices() {
+        Log.v("MY_MAPS", "Starting Locations Services Called");
+
+        try {
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, req, this);
+            Log.v("MY_MAPS", "Requesting location updates");
+        } catch (SecurityException exception) {
+            // Toast to enable location
+            Log.v("MY_MAPS", exception.toString());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationServices();
+                    Log.v("MY_MAPS", "Permission granted - starting services");
+                } else {
+                    // Toast to enable location
+                    Log.v("MY_MAPS", "Permission Denied");
+                    Toast.makeText(this, "Please enable location so we can check your weather.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
